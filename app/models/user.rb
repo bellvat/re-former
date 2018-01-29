980688c4b504f9a84c0b658aad282474e21a223b
bellvat <msirathanarun@gmail.com>
2018-01-28 17:53:38 -0800

    Added bcrypt and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
   validates :username, presence: true
   validates :email, presence: true
+  has_secure_password
   validates :password, presence: true
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(id: 1, username: "example user", email: "user@example.com",
+                      password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be saved as lowercase" do
+    mixed_case = "Hello@gmail.com"
+    @user.email = mixed_case
+    @user.save
+    assert_equal mixed_case.downcase, @user.reload.email
+  end
 end
